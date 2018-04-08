d8b2225941434d06dd5f2f2474a3337576702081
unlocker-sy <soonyoung87@gmail.com>
2018-04-08 17:07:35 +0900

    openai_gym - frozen lake예제 추가 tensorflow - 동작 확인 샘플코드 추가 keras - study 발표할 내용 정리(진행 중)

---
+++ tensorflow/hello_gym.py
@@ -0,0 +1,10 @@
+import gym
+
+env = gym.make('FrozenLake-v0')
+
+observation = env.reset()
+
+for _ in range(1000):
+    env.render()
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
